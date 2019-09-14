fb7ab7896cddf8275d65d4097822be5fb6b7708a
Kevin O'Neil <kevinmoneil@live.com>
2019-09-14 18:19:24 +0900

    removes compiled file

--- server/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var app = express_1["default"]();
-var port = 3000;
-app.listen(port, function () {
-    console.log("Listening on port " + port);
-});
