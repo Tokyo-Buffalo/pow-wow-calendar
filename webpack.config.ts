88c909bd17d9ed608aa173bb6dd437abd47236e9
Kevin O'Neil <kevinmoneil@live.com>
2019-09-14 22:56:22 +0900

    adds webpack config file

---
+++ webpack.config.ts
@@ -0,0 +1,22 @@
+import path from "path";
+import webpack from "webpack";
+
+export default ({
+  mode: "development",
+  entry: './client/src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'foo.bundle.js'
+  },
+  watch: true, 
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
+  },
+  module: {
+    rules: [{
+      test: /\.tsx?$/,
+      use: "ts-loader",
+      exclude: /\.test.tsx?$/,=
+    }]
+  }
+});
