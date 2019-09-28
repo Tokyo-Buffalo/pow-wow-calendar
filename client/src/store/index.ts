2405000e361481970b47a87b1a38ffd1459f3132
Kevin O'Neil <kevinmoneil@live.com>
2019-09-29 00:50:47 +0900

    creates main store file with redux-ts-utils

---
+++ client/src/store/index.ts
@@ -0,0 +1,24 @@
+import { createStore, Store } from 'redux';
+import { createAction, handleAction, reduceReducers } from 'redux-ts-utils';
+
+const setUserLoginStatus = createAction<boolean>("setUserLoginStatus");
+
+interface State {
+  readonly user: {
+    isLoggedIn: boolean;
+  }
+};
+
+const initialState: State = {
+  user: {
+    isLoggedIn: false
+  },
+};
+
+const reducer = reduceReducers<State>([
+  handleAction(setUserLoginStatus, (state, { payload }) => {
+    state.user.isLoggedIn = payload;
+  })
+]);
+
+export const store: Store<State> = createStore(reducer);
