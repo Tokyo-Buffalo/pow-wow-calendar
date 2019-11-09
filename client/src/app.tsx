7a69c59a965d83f10fb4463c223c15837d8f061b
Kevin O'Neil <kevinmoneil@live.com>
2019-11-10 01:32:55 +0900

    forammting works

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,6 +1,6 @@
 import { hot } from "react-hot-loader/root";
 import React, { Fragment } from "react";
-import { setConfig } from 'react-hot-loader';
+import { setConfig } from "react-hot-loader";
 import "antd/dist/antd.css";
 
 import { Button } from "antd";
@@ -8,7 +8,9 @@ import { Button } from "antd";
 const App = () => (
   <Fragment>
     <h1>Sign in</h1>
-    <Button type="primary" href="/auth/google">Sign in with Google</Button>
+    <Button type="primary" href="/auth/google">
+      Sign in with Google
+    </Button>
   </Fragment>
 );
 
