a7109b109f715836a71b7df3e9a354e7332ded1b
Larry B <larrybuffaloboy@gmail.com>
2019-11-18 18:19:44 -0500

    Attempting to add logo to the page

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { Layout, Menu } from 'antd';
 import { Link } from 'react-router-dom';
 
+const logo = require('../common/images/powLogo.svg')
 const { Header } = Layout;
 
 export const AppHeader = () => (
   <Header>
     <Menu theme="dark" mode="horizontal" style={{ lineHeight: '64px' }}>
       <Menu.Item key="home">
-        <Link to="/">Home</Link>
+        <Link to="/">
+          <img src={logo}></img>
+        </Link>
       </Menu.Item>
       <Menu.Item key="events">
         <Link to="/events">Events</Link>
