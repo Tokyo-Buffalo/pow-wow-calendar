6b24b29c49bf4849e5c39909d703503e4f174ccf
Larry B <larrybuffaloboy@gmail.com>
2019-11-21 13:38:26 -0500

    css changes, working on layout

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Layout, Menu } from 'antd';
 import { Link } from 'react-router-dom';
+import './Header.css';
 
 import Logo from '../components/Logo';
 
@@ -8,10 +9,13 @@ const { Header } = Layout;
 
 export const AppHeader = () => (
   <Header>
-    <Menu theme="dark" mode="horizontal" style={{ lineHeight: '64px' }}>
+      <div className="logo">
+          <Logo width="200px" />
+      </div>
+    <Menu theme="light" mode="horizontal" style={{ lineHeight: '64px' }}>
+       
       <Menu.Item key="home">
         <Link to="/">
-          <Logo width="100px" />
         </Link>
       </Menu.Item>
       <Menu.Item key="events">
--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Layout } from 'antd';
+import { Layout, Breadcrumb } from 'antd';
 import { Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
@@ -18,12 +18,19 @@ export const AppLayout = () => {
   });
 
   return (
-    <Layout>
+    <Layout className="layout">
       <AppHeader />
-      <Content>
-        {routes.map((route, i) => (
-          <Route key={i} {...route} />
-        ))}
+      <Content  style={{ padding: '0 50px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+          {routes.map((route, i) => (
+            <Route key={i} {...route} />
+          ))}
+        </div>
       </Content>
       <AppFooter />
     </Layout>
