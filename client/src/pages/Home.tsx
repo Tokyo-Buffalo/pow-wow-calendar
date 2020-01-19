b15ef7a8f2ba42c51bb9c398c5a8b3cc0fa57862
Kevin O'Neil <kevinmoneil@live.com>
2020-01-18 20:39:22 -0800

    Adds check to see if user is logged in

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,21 @@
-import React, { Fragment } from 'react';
-import { Button } from 'antd';
+import React, { Fragment } from "react";
+import { Button } from "antd";
+import { useSelector } from "react-redux";
+import { IState } from "../store";
 
-export const Home = () => (
-  <Fragment>
-    <h1>Sign in</h1>
-    <Button type="primary" href="/auth/google">
-      Sign in with Google
-    </Button>
-  </Fragment>
-);
+export const Home = () => {
+  const { hasLoaded, isLoggedIn } = useSelector((state: IState) => state.user);
+
+  return (
+    <Fragment>
+      {hasLoaded && !isLoggedIn && (
+        <Fragment>
+          <h1>Sign in</h1>
+          <Button type="primary" href="/auth/google">
+            Sign in with Google
+          </Button>
+        </Fragment>
+      )}
+    </Fragment>
+  );
+};
