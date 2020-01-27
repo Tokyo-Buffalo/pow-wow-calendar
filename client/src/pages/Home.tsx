b0f470cde49f1a7b4fd76abd4d39e23674dcb76a
Kevin O'Neil <kevinmoneil@live.com>
2020-01-26 20:31:19 -0800

    changes conditional

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -14,16 +14,16 @@ export const Home = () => {
 
   return (
     <Fragment>
-      {!isLoggedIn ? (
+      {isLoggedIn ? (
+        <Fragment>
+          <h1>Welcome back!</h1>
+        </Fragment>
+      ) : (
         <Fragment>
           <h1>Sign in</h1>
           <Button type="primary" href="/auth/google">
             Sign in with Google
           </Button>
-        </Fragment>
-      ) : (
-        <Fragment>
-          <h1>Welcome back!</h1>
         </Fragment>
       )}
     </Fragment>
