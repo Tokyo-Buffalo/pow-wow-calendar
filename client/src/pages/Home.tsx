2680b64562bca021b56d4dad8120e9ab5aea05ba
Kevin O'Neil <kevinmoneil@live.com>
2020-01-26 19:55:26 -0800

    Returns early if hasLoaded is false

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,26 +1,31 @@
 import React, { Fragment } from "react";
-import { Button } from "antd";
+import { Button, Layout } from "antd";
 import { useSelector } from "react-redux";
 import { IState } from "../store";
+
+const { Content } = Layout;
 
 export const Home = () => {
   const { hasLoaded, isLoggedIn } = useSelector((state: IState) => state.user);
 
+  if (!hasLoaded) {
+    return null;
+  }
+
   return (
-    <Fragment>
-      {hasLoaded && !isLoggedIn && (
+    <Content style={{ padding: "25px 50px" }}>
+      {!isLoggedIn ? (
         <Fragment>
           <h1>Sign in</h1>
           <Button type="primary" href="/auth/google">
             Sign in with Google
           </Button>
         </Fragment>
-      )}
-      {hasLoaded && isLoggedIn && (
+      ) : (
         <Fragment>
           <h1>Welcome back!</h1>
         </Fragment>
       )}
-    </Fragment>
+    </Content>
   );
 };
