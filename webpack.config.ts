ee19fdda3cf04bdb1998dd7b5c2ff1a05638413b
Kevin O'Neil <kevinmoneil@live.com>
2020-02-20 21:09:40 -0800

    adds publicPath to avoid routing issue

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,36 +1,37 @@
-import path from 'path';
-import webpack from 'webpack';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from "path";
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
 export default {
-  mode: 'development' as 'development',
-  entry: ['webpack-hot-middleware/client', 'react-hot-loader/patch', './client/src/index.tsx'],
+  mode: "development" as "development",
+  entry: ["webpack-hot-middleware/client", "react-hot-loader/patch", "./client/src/index.tsx"],
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'build.js'
+    publicPath: "/",
+    path: path.resolve(__dirname, "dist"),
+    filename: "build.js"
   },
   watch: true,
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx']
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /\.test.tsx?$/
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ["style-loader", "css-loader"]
       }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'assets/index.html')
+      template: path.resolve(__dirname, "assets/index.html")
     }),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.EnvironmentPlugin(['NODE_ENV', 'BASE_URI'])
+    new webpack.EnvironmentPlugin(["NODE_ENV", "BASE_URI"])
   ]
 };
