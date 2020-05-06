427d1b4e720ef01792aca141e142d0116f8fa62e
Kevin O'Neil <kevinmoneil@live.com>
2020-05-05 22:34:06 -0700

    another test

--- server/controllers/__tests__/User.test.ts
+++ server/controllers/__tests__/User.test.ts
@@ -2,18 +2,17 @@
 const { User } = require("../User");
 import { pool } from "./../../db/pool";
 
-// const user = new User();
+const user = new User();
 
-// const sub = "123456789";
-// const email = "tester@test.com";
-// const picture = "https://google.com/picture.jpg";
+const sub = "123456789";
+const email = "tester@test.com";
+const picture = "https://google.com/picture.jpg";
 
 test("Creates a new user", async () => {
-  expect(true).toEqual(true);
-  // await user.createUser(sub, email, picture);
+  await user.createUser(sub, email, picture);
 
-  // await user.findUser(sub).then(async (res: boolean) => {
-  //   await expect(res).toEqual(true);
-  // });
-  // return await pool.end();
+  await user.findUser(sub).then(async (res: boolean) => {
+    await expect(res).toEqual(true);
+  });
+  return await pool.end();
 });
