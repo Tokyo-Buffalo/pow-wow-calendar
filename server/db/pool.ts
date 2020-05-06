271fe7f40cdbe519cbd4c0e2d01f3943c0242fe5
Kevin O'Neil <kevinmoneil@live.com>
2020-05-05 21:12:26 -0700

    Adds test db port

--- server/db/pool.ts
+++ server/db/pool.ts
@@ -3,7 +3,16 @@ import { config as loadDotenvConfig } from "dotenv";
 
 loadDotenvConfig();
 
-const { PGHOST, PGHOST_TEST, PGDATABASE, PGDATABASE_TEST, PGUSER, PGUSER_TEST } = process.env;
+const {
+  PGHOST,
+  PGHOST_TEST,
+  PGDATABASE,
+  PGDATABASE_TEST,
+  PGUSER,
+  PGUSER_TEST,
+  PGPASSWORD_TEST,
+  POSTGRES_PORT,
+} = process.env;
 
 const isTestEnv = process.env.NODE_ENV === "test";
 
@@ -11,4 +20,6 @@ export const pool = new Pool({
   host: isTestEnv ? PGHOST_TEST : PGHOST,
   database: isTestEnv ? PGDATABASE_TEST : PGDATABASE,
   user: isTestEnv ? PGUSER_TEST : PGUSER,
+  password: PGPASSWORD_TEST,
+  port: parseInt(POSTGRES_PORT, 10),
 });
