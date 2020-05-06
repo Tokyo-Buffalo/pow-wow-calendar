5d29248b69d9c61d18a1fba0caaa3d7b9966e357
Kevin O'Neil <kevinmoneil@live.com>
2020-05-05 22:16:00 -0700

    Another CI test

--- server/controllers/__tests__/User.test.ts
+++ server/controllers/__tests__/User.test.ts
@@ -2,23 +2,23 @@
 const { User } = require("../User");
 import { pool } from "./../../db/pool";
 
-const user = new User();
+// const user = new User();
 
-const sub = "123456789";
-const email = "tester@test.com";
-const picture = "https://google.com/picture.jpg";
+// const sub = "123456789";
+// const email = "tester@test.com";
+// const picture = "https://google.com/picture.jpg";
 
-beforeEach(async () => {
-  await pool.query(`
-    CREATE TABLE IF NOT EXISTS users(
-      id VARCHAR(128) PRIMARY KEY,
-      email VARCHAR(128) NOT NULL, 
-      picture VARCHAR(128) NOT NULL,
-      created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
-      modified_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-      is_admin BOOL DEFAULT FALSE
-  `);
-});
+// beforeEach(async () => {
+// await pool.query(`
+//   CREATE TABLE IF NOT EXISTS users(
+//     id VARCHAR(128) PRIMARY KEY,
+//     email VARCHAR(128) NOT NULL,
+//     picture VARCHAR(128) NOT NULL,
+//     created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+//     modified_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+//     is_admin BOOL DEFAULT FALSE
+// `);
+// });
 
 // afterEach(async () => {
 //   await pool.query("TRUNCATE TABLE users;");
