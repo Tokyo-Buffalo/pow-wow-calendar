ce934219ec0f19738f9ee57d149933ab5f808c5f
Kevin O'Neil <kevinmoneil@live.com>
2020-05-05 20:38:07 -0700

    Adds ternary check for test db env

--- server/db/pool.ts
+++ server/db/pool.ts
@@ -3,13 +3,12 @@ import { config as loadDotenvConfig } from "dotenv";
 
 loadDotenvConfig();
 
-const { PGDATABASE, PGDATABASE_TEST } = process.env;
+const { PGHOST, PGHOST_TEST, PGDATABASE, PGDATABASE_TEST, PGUSER, PGUSER_TEST } = process.env;
 
 const isTestEnv = process.env.NODE_ENV === "test";
-const database = isTestEnv ? PGDATABASE_TEST : PGDATABASE;
 
 export const pool = new Pool({
-  host: process.env.PGHOST,
-  database,
-  user: process.env.PGUSER,
+  host: isTestEnv ? PGHOST_TEST : PGHOST,
+  database: isTestEnv ? PGDATABASE_TEST : PGDATABASE,
+  user: isTestEnv ? PGUSER_TEST : PGUSER,
 });
