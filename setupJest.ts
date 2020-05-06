dbe73d8c77ab85b25facef56e36d3e46419dc8bc
Kevin O'Neil <kevinmoneil@live.com>
2020-05-05 23:13:05 -0700

    log hi just to check if it runs

--- setupJest.ts
+++ setupJest.ts
@@ -4,3 +4,4 @@ require("jest-fetch-mock").enableMocks();
 
 const client = new Client(/*connectionInfo*/);
 client.on("drain", client.end.bind(client)); //auto disconnect client after last query ends
+console.log("hiiiiiiiiiiiiiiiiiiiiiiii");
