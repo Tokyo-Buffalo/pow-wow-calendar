1dd1b3af57b0c41b9df54d43f8f4a6208e1b7528
Kevin O'Neil <kevinmoneil@live.com>
2020-05-17 14:02:23 -0700

    Adds PWCEvent test for controller

---
+++ server/controllers/Events.ts
@@ -0,0 +1,28 @@
+import { pool } from "db/pool";
+
+interface PWCEvent {
+  name: string;
+  description: string;
+  location: string;
+  startDate: string;
+  endDate: string;
+  createdAt: string;
+  updatedAt: string;
+  createdBy: string;
+  attendees: Array<{}>;
+}
+
+type EventsType = Array<PWCEvent>;
+
+export class PWCEvents {
+  async getAllEvents(): Promise<EventsType | Error> {
+    const query = "SELECT * FROM events;";
+
+    try {
+      const events = await pool.query(query);
+      return events.rows[0];
+    } catch {
+      return new Error("Could not retrieve events");
+    }
+  }
+}
