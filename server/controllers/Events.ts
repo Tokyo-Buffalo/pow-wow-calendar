b4cf13ad9cf8eac76a8b7e6d9825c59640154be6
Kevin O'Neil <kevinmoneil@live.com>
2020-05-18 01:15:52 -0700

    adds uuid package, adds create event args type and method

--- server/controllers/Events.ts
+++ server/controllers/Events.ts
@@ -1,16 +1,21 @@
 import { pool } from "db/pool";
+import { v4 as uuidv4 } from "uuid";
 
-interface PWCEvent {
+interface CreateEventArgs {
   name: string;
   description: string;
   location: string;
   start_date: string;
   end_date: string;
+  attendees: Array<string>;
+  organizers: Array<string>;
+}
+
+interface PWCEvent extends CreateEventArgs {
+  id: string;
   created_date: string;
   modified_date: string;
   created_by: string;
-  attendees: Array<string>;
-  organizers: Array<string>;
 }
 
 type EventsType = Array<PWCEvent>;
@@ -29,6 +34,19 @@ export class PWCEvents {
       return events.rows;
     } catch {
       return new Error("Could not retrieve events");
+    }
+  }
+
+  async createEvent(params: CreateEventArgs, created_by: string) {
+    const id = uuidv4();
+    const paramsArray = Object.values(params);
+    const query =
+      "INSERT INTO events(id, name, description, location, start_date, end_date, created_by, attendees, organizers) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *;";
+
+    try {
+      return await pool.query(query, [id, ...paramsArray, created_by]);
+    } catch {
+      return new Error("Could not save event");
     }
   }
 }
